ee1416c6fc290e97ed8efc72d9a6ea5ceb2b7a50
gangel27 <georgeangeluk@gmail.com>
2024-08-20 18:22:33 +0100

    filter system stuff

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
+import json
+
+# sizes go T/S/M/L/H
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "aslfjivnw"
@@ -38,6 +42,27 @@ def get_icon(filename):
 @app.route("/Images/Stock_Images/<filename>")
 def get_stock_img(filename):
     return send_from_directory(app.config["SITE_STOCK_IMG_DEST"], filename)
+
+
+@app.route("/filter/<sex>+<size>", methods=['GET', 'POST'])
+def return_filter_items(sex, size): 
+    img = 'Images/Stock_Images/'
+    title = 'TITTTLE'
+    subtitle = 'this is an example of what a subtitle would look like. and more flabberjabber'
+
+    with open('stock.json') as json_file: 
+        data = json.load(json_file) ['stock']
+    images = []
+    titles = []
+    subtitles = []
+    for product in data: 
+        if (product['size'] in size or size == ".") and (sex == "." or sex == product['sex']): 
+            images.append(img + product['thumbnail'])
+            titles.append(product['title'])
+            subtitles.append(product['subtitle'])
+
+    return jsonify([images, titles, subtitles])
+    
 
 
 if __name__ == "__main__":
--- static/gallery_script.js
+++ static/gallery_script.js
@@ -1,31 +1,119 @@
-let filters = document.getElementById("filters")
-let gallery_stock = document.getElementById("gallery_display")
+const filters = document.getElementById("filters")
+const gallery_stock = document.getElementById("gallery_display")
+
 
 
 // this will later be incorperated with the back end 
-let no_results = 4; 
-let white_hoodie_img_file = 'Images/Stock_Images/Hoodie.png'
+let white_hoodie_img_file = 'Images/Stock_Images/Hoodie_black.png'
 let title_text = "White Hoody"
 let subtitle_text = "This is a beautiful white hoodie for men. X, Xl, M, S"
+// we just need it to return a list of everything checked
 
-for (let i=0; i < gallery_stock.children.length; i++){
-    let card = gallery_stock.children[i].children[0]
-    if (i >= no_results){
-        gallery_stock.children[i].style.display = 'none'
-    } else { 
-        let card_image = card.getElementsByTagName("img")[0]
-        let card_caption = card.getElementsByTagName("div")[0]
-        let card_title = card_caption.getElementsByTagName("h5")[0]
-        let card_subtitle = card_caption.getElementsByTagName("p")[0]
-        let card_submit = card_caption.getElementsByTagName("button")[0]
+let images = [white_hoodie_img_file, white_hoodie_img_file, white_hoodie_img_file, white_hoodie_img_file];
+let titles = [title_text, title_text, title_text, title_text]; 
+let subtitle_texts = [subtitle_text, subtitle_text, subtitle_text, subtitle_text]
 
-        card_image.src = white_hoodie_img_file;
-        card_title.innerHTML = title_text; 
-        card_subtitle.innerHTML = subtitle_text; 
-        card_submit.onclick = function() {
-            console.log(i)
-            console.log(card_title.innerHTML);
+
+let no_results = images.length;
+
+function display_search_results(images, titles, subtitles) {
+    let no_results = images.length; 
+
+    for (let i=0; i < gallery_stock.children.length; i++){
+        let card = gallery_stock.children[i].children[0]
+        if (i >= no_results){
+            gallery_stock.children[i].style.display = 'none'
+        } else { 
+            let card_image = card.getElementsByTagName("img")[0]
+            let card_caption = card.getElementsByTagName("div")[0]
+            let card_title = card_caption.getElementsByTagName("h5")[0]
+            let card_subtitle = card_caption.getElementsByTagName("p")[0]
+            let card_submit = card_caption.getElementsByTagName("button")[0]
+    
+            card_image.src = images[i];
+            card_title.innerHTML = titles[i]; 
+            card_subtitle.innerHTML = subtitles[i]; 
+            card_submit.onclick = function() {
+                console.log(i)
+                // buying functino to go here
+            }
         }
-        console.log(card_caption)
     }
+    
 }
+
+async function query_search_results(sex, size) { 
+    console.log(sex )
+    console.log(size)
+    let images = []
+    let titles = []
+    let subtitles = []
+    
+    await fetch(`/filter/${sex}+${size}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();  // Assuming the response is in JSON format
+        })
+        .then(data => {
+            // Handle the data received from the server
+            images = data[0]
+            titles = data[1]
+            subtitles = data[2]
+            display_search_results(images, titles, subtitles)
+            
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+    
+}
+
+function get_filter_data() { 
+    let sex = '.'
+    let size = '.'
+    let isMan = document.getElementById('ManCheck').checked;
+    let isWoman = document.getElementById('WomanCheck').checked;
+    let isXS = document.getElementById('XSCheck').checked;
+    let isS = document.getElementById('SCheck').checked;
+    let isM = document.getElementById('MCheck').checked;
+    let isL = document.getElementById('LCheck').checked;
+    let isXL = document.getElementById('XLCheck').checked;
+
+    if (isMan && !isWoman) { 
+        sex = 'M'
+    } else if (isWoman && !isMan) { 
+        sex = 'F'
+    }
+
+    if (!isXS || !isS || !isM || !isM || !isL || !isXL){
+        size = ''
+        if (isXS) { 
+            size += 'XS'
+        }
+        if (isS) { 
+            size += 'S'
+        }
+        if (isM) { 
+            size += 'M'
+        }
+        if (isL) { 
+            size += 'L'
+        }
+        if (isXL) { 
+            size += 'XL'
+        }
+    }
+
+    console.log(sex)
+    console.log(size)
+    return [sex, size]
+}
+
+display_search_results(images, titles, subtitle_texts)
+query_search_results()
+filters.addEventListener('change', function() { 
+    let data = get_filter_data()
+    data = query_search_results(data[0], data[1])
+})

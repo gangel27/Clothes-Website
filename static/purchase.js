5022600b344812d2f5ea3bc6cad82b49826d6bd9
gangel27 <georgeangeluk@gmail.com>
2024-08-25 14:00:35 +0100

    restyling main screen
    backgruond images really suck

--- static/purchase.js
+++ static/purchase.js
@@ -1,1 +1,31 @@
 console.log('this is the purchase screen')
+
+window.addEventListener('load', function() { 
+    const prodSize = document.getElementById('productSize')
+    prodSize.innerHTML = size_convert(prodSize.innerHTML)
+})
+
+function size_convert(size) { 
+    val = '' 
+    switch (size) {
+        case "T":
+            val = 'Extra Small'
+            break;
+        case "S":
+            val = 'Small'
+            break;
+        case "M":
+            val = 'Medium'
+            break;
+        case "L":
+            val = 'Large'
+            break;
+        case "H":
+            val = 'Extra Large'
+            break;    
+        default:
+            break;
+    }
+    return val 
+}
+
